8f6aa67821a6e903486cb42ad46f4f4131e252be
Lukas Pelz <lukas.pelz@posteo.de>
2020-11-02 22:36:24 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/triplog/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.triplog
+
+import android.os.Bundle
+import android.util.Log
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import nl.joery.animatedbottombar.AnimatedBottomBar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (supportActionBar != null)
+            supportActionBar?.hide()
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment.newInstance("null", "null")).commit()
+
+        bottom_bar.onTabSelected = {
+            val newFragment: Fragment? = when (it.id) {
+                R.id.home -> HomeFragment.newInstance("null", "null")
+                R.id.settings -> SettingsFragment.newInstance("null", "null")
+                R.id.map -> MapFragment.newInstance("null", "null")
+                else -> null
+            }
+            if (newFragment != null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, newFragment).commit()
+            }
+        }
+
+
+    }
+
+    private fun initNavBar() {
+        bottom_bar.setOnTabSelectListener(object : AnimatedBottomBar.OnTabSelectListener {
+            override fun onTabSelected(
+                lastIndex: Int,
+                lastTab: AnimatedBottomBar.Tab?,
+                newIndex: Int,
+                newTab: AnimatedBottomBar.Tab
+
+            ) {
+                Log.d("bottom_bar", "Selected index: $newIndex, title: ${newTab.title}")
+            }
+
+            // An optional method that will be fired whenever an already selected tab has been selected again.
+            override fun onTabReselected(index: Int, tab: AnimatedBottomBar.Tab) {
+                Log.d("bottom_bar", "Reselected index: $index, title: ${tab.title}")
+            }
+        })
+    }
+}
