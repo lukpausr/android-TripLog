04a472ce5e5ec6923874ad7388e3474d0730a73c
Lukas Pelz <lukas.pelz@posteo.de>
2020-11-23 13:46:16 +0100

    Class setup for Permission Handling

--- app/src/main/java/com/dhbw/triplog/ui/fragments/TripFragment.kt
+++ app/src/main/java/com/dhbw/triplog/ui/fragments/TripFragment.kt
@@ -1,7 +1,42 @@
 package com.dhbw.triplog.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.dhbw.triplog.R
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_trip.*
+import pub.devrel.easypermissions.EasyPermissions
 
-class TripFragment : Fragment(R.layout.fragment_trip) {
+@AndroidEntryPoint
+class TripFragment : Fragment(R.layout.fragment_trip), EasyPermissions.PermissionCallbacks {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun setupRecyclerView() = rvTrips.apply {
+        TODO("Not yet implemented")
+    }
+
+    private fun requestPermissions() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        TODO("Not yet implemented")
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        TODO("Not yet implemented")
+    }
 }
