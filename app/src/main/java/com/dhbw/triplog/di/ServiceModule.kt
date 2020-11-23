47094d3c11442fee42c30a6b5be61e60f296adc1
Lukas Pelz <lukas.pelz@posteo.de>
2020-11-23 13:38:52 +0100

    Google Maps API and Google Playservices API, API Keys added to .gitignore

---
+++ app/src/main/java/com/dhbw/triplog/di/ServiceModule.kt
@@ -0,0 +1,22 @@
+package com.dhbw.triplog.di
+
+import android.content.Context
+import com.google.android.gms.location.FusedLocationProviderClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideFusedLocationProviderClient(
+            @ApplicationContext app: Context
+    ) = FusedLocationProviderClient(app)
+
+}
