eb1515ea20d34ffe026fb4ec5b0770fe772dd64d
Lukas Pelz <lukas.pelz@posteo.de>
2020-11-28 15:07:01 +0100

    Test

--- app/src/main/java/com/dhbw/triplog/ui/MainActivity.kt
+++ app/src/main/java/com/dhbw/triplog/ui/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
 
+
         bottomNavView.setupWithNavController(navHostFragment.findNavController())
         bottomNavView.setOnSystemUiVisibilityChangeListener {  }
 
