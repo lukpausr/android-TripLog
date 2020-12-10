1c10ab231bca1d69f6d357859712f7c10538ce97
Lukas Pelz <lukas.pelz@posteo.de>
2020-12-10 20:06:50 +0100

    Data Export to CSV + Label in fileName

--- app/src/main/java/com/dhbw/triplog/other/DataExportUtility.kt
+++ app/src/main/java/com/dhbw/triplog/other/DataExportUtility.kt
@@ -1,16 +1,56 @@
 package com.dhbw.triplog.other
 
+import android.location.Location
+import android.util.Log
 import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
+import java.io.File
+import java.lang.StringBuilder
+import java.text.SimpleDateFormat
 
 object DataExportUtility {
-    fun writeGPSDataToFile() {
-        val row1 = listOf("a", "b", "c")
-        val row2 = listOf("d", "e", "f")
-        csvWriter().open("test.csv") {
-            writeRow(row1)
-            writeRow(row2)
-            writeRow("g", "h", "i")
-            writeRows(listOf(row1, row2))
+
+    fun writeGPSDataToFile(file: File, gpsPoints: MutableList<Location>, selectedTransportType: Labels?) {
+
+        val labels = labelsToString(selectedTransportType)
+
+        val filename =  file.absolutePath +
+                        "/" +
+                        System.currentTimeMillis().toString() + labels
+
+        Log.d("filename", filename)
+
+        val simpleDateFormat = SimpleDateFormat("yyyy:MM:dd:HH:mm:ss:SS:z")
+
+        csvWriter().open("$filename.csv") {
+            writeRow("Time", "Time_in_s", "Latitude", "Longitude", "Altitude", "Speed")
+            for (gpsPoint in gpsPoints) {
+                writeRow(
+                        simpleDateFormat.format(gpsPoint.time),
+                        (gpsPoint.time/1000).toString(),
+                        gpsPoint.latitude.toString(),
+                        gpsPoint.longitude.toString(),
+                        gpsPoint.altitude.toString(),
+                        gpsPoint.speed.toString()
+                )
+            }
         }
+
     }
+
+    private fun labelsToString(selectedTransportType: Labels?) : String {
+        val stringBuilder = StringBuilder()
+        if(selectedTransportType?.label != "") {
+            stringBuilder.append("_" + selectedTransportType?.label)
+        }
+        if(selectedTransportType?.subLabel != "") {
+            stringBuilder.append("_" + selectedTransportType?.subLabel)
+        }
+        if(selectedTransportType?.subSubLabel != "") {
+            stringBuilder.append("_" + selectedTransportType?.subSubLabel)
+        }
+        return stringBuilder.toString()
+    }
+
+
+
 }
--- app/src/main/java/com/dhbw/triplog/ui/fragments/TripFragment.kt
+++ app/src/main/java/com/dhbw/triplog/ui/fragments/TripFragment.kt
@@ -12,7 +12,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupWindow
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -173,6 +172,8 @@ class TripFragment : Fragment(R.layout.fragment_trip), EasyPermissions.Permissio
     private fun stopTracking() {
         sendCommandToService(ACTION_STOP_SERVICE)
 
+        saveGPSDataAsFile()
+
         gpsPointsLatLng.clear()
         gpsPoints.clear()
         map?.clear()
@@ -204,7 +205,10 @@ class TripFragment : Fragment(R.layout.fragment_trip), EasyPermissions.Permissio
     }
 
     private fun saveGPSDataAsFile () {
-        DataExportUtility.writeGPSDataToFile()
+        val path = context?.filesDir
+        if (path != null) {
+            DataExportUtility.writeGPSDataToFile(path, gpsPoints, selectedTransportType)
+        }
     }
 
 
