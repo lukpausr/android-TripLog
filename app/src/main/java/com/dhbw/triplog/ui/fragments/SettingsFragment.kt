5a3710442c78dd8d3db57baac1f06775495414e9
Lukas Pelz <lukas.pelz@posteo.de>
2020-12-22 10:05:08 +0100

    Show UserID in Settings Fragment

--- app/src/main/java/com/dhbw/triplog/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/dhbw/triplog/ui/fragments/SettingsFragment.kt
@@ -1,7 +1,25 @@
 package com.dhbw.triplog.ui.fragments
 
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.dhbw.triplog.R
+import com.dhbw.triplog.other.DeviceRandomUUID
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_settings.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    @Inject
+    lateinit var sharedPref: SharedPreferences
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvUserID.text = DeviceRandomUUID.getRUUID(sharedPref)
+    }
+
 }
