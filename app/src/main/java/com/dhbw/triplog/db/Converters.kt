355aed68ca3dca6665babfd5889ecc88964a7c9b
Lukas Pelz <lukas.pelz@posteo.de>
2020-12-28 12:40:12 +0100

    Code Documentation

--- app/src/main/java/com/dhbw/triplog/db/Converters.kt
+++ app/src/main/java/com/dhbw/triplog/db/Converters.kt
@@ -5,8 +5,18 @@ import android.graphics.BitmapFactory
 import androidx.room.TypeConverter
 import java.io.ByteArrayOutputStream
 
+/**
+ * Converter class being used to do necessary type conversions before saving something in the DB
+ */
 class Converters {
 
+    /**
+     * Converting Bitmap to ByteArray
+     *
+     * @param bmp Bitmap type object like Google Maps Screenshot
+     *
+     * @return ByteArray representation of Bitmap
+     */
     @TypeConverter
     fun fromBitmap(bmp: Bitmap) : ByteArray {
         val outputStream = ByteArrayOutputStream()
@@ -14,9 +24,15 @@ class Converters {
         return outputStream.toByteArray()
     }
 
+    /**
+     * Converting ByteArray to Bitmap
+     *
+     * @param bytes ByteArray representation of a Bitmap
+     *
+     * @return Bitmap back-conversion of ByteArray
+     */
     @TypeConverter
     fun toBitmap(bytes: ByteArray) : Bitmap {
         return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
     }
-
 }
