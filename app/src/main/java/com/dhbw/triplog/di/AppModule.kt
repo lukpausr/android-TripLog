fb88b9f6d5166f0cf06c46c6fd44df8be707e985
Lukas Pelz <lukas.pelz@posteo.de>
2020-12-28 12:36:22 +0100

    Optimized imports

--- app/src/main/java/com/dhbw/triplog/di/AppModule.kt
+++ app/src/main/java/com/dhbw/triplog/di/AppModule.kt
@@ -2,20 +2,15 @@ package com.dhbw.triplog.di
 
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
-import android.content.SharedPreferences
 import androidx.room.Room
 import com.dhbw.triplog.db.TripDatabase
-import com.dhbw.triplog.other.Constants.KEY_DSGVO
 import com.dhbw.triplog.other.Constants.SHARED_PREFERENCES_NAME
 import com.dhbw.triplog.other.Constants.TRIP_DATABASE_NAME
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.android.scopes.ServiceScoped
 import javax.inject.Singleton
 
 /**
